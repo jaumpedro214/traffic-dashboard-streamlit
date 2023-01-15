31787395c1fd97d9b18b99b0fe5f88fb88120280
joaopedro214 <joaopedrodasilvalima@gmail.com>
2023-01-15 08:45:49 -0300

    FEAT: main.py streamlit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,29 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("My first app")
+st.title("Traffic in Belo Horizonte - MG")
+
+# calendar 
+st.date_input("Select a date", pd.to_datetime("2019-01-01"))
+
+# Slider with date
+st.slider(
+    "Select a range of values", 
+    pd.datetime(2019, 1, 1), 
+    pd.datetime(2019, 1, 31),
+    (pd.datetime(2019, 1, 1), pd.datetime(2019, 1, 31))
+)
+
+# slider with the hour and minute
+columns = st.columns([1,20,1])
+columns[0].write(":sunrise:")
+columns[1].slider(
+    "",
+    pd.datetime(2019, 1, 1, 0, 0),
+    pd.datetime(2019, 1, 1, 23, 59),
+    (pd.datetime(2019, 1, 1, 0, 0), pd.datetime(2019, 1, 1, 23, 59)),
+    format="HH:mm",
+    label_visibility = "collapsed"
+)
+columns[2].write(":night_with_stars:")
