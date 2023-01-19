335fba40bede0ed6dfad1d0f5bec3ab09f31021f
joaopedro214 <joaopedrodasilvalima@gmail.com>
2023-01-19 07:50:56 -0300

    FEAT: Plot maps & Georeferenced data

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 import streamlit as st
-import pandas as pd
+
+import geopandas as gpd
+import contextily as cx
+
 import numpy as np
+import pandas as pd
+import pyarrow.parquet as pq
 
 MIN_DATE = pd.to_datetime("2022-01-01")
 MAX_DATE = pd.to_datetime("2022-02-28")
@@ -50,11 +55,9 @@ def hour_range():
     return min_hour, max_hour
 
 
-def read_data(
+def read_traffic_count_data(
     min_date, max_date, min_hour, max_hour, vehicle_classes
 ):
-    df_traffic = pd.read_parquet("/data/vehicles_count.parquet")
-
     # translate the vehicle classes
     vehicle_classes_translate = {
         "BUS/TRUCK": "CAMINHAO_ONIBUS",
@@ -68,10 +71,19 @@ def read_data(
         for vehicle_class in vehicle_classes
     ]
 
-    # filter by vehicle class
-    df_traffic = df_traffic.query(
-        "CLASS in @vehicle_classes"
-    )
+    # Create all month numbers between min_date and max_date
+    month_numbers = np.arange(
+        min_date.month, max_date.month + 1
+    ).tolist()
+
+    # Use pyarrow to open the parquet file
+    df_traffic = pq.ParquetDataset(
+        "/data/vehicles_count.parquet",
+        filters=[
+            ("MONTH", "in", month_numbers),
+            ("CLASS", "in", vehicle_classes),
+        ]
+    ).read_pandas().to_pandas()
 
     # filter by date
     df_traffic = df_traffic.query(
@@ -91,10 +103,54 @@ def read_data(
         .agg({"COUNT": "sum"})
         .reset_index()
     )
-
-    st.dataframe(df_traffic)
+    # LATITUDE and LONGITUDE TO FLOAT
+    df_traffic["LATITUDE"] = df_traffic["LATITUDE"].astype(float)
+    df_traffic["LONGITUDE"] = df_traffic["LONGITUDE"].astype(float)
 
     return df_traffic
+
+
+def read_map_data(df_traffic):
+    # read geojson
+    path = './map/mg.json'
+    gdf_bh = gpd.read_file(path)
+
+    gdf_bh = gdf_bh.loc[gdf_bh["name"] == "Belo Horizonte"]
+    gdf_bh = gdf_bh.to_crs(epsg=3857)
+
+    ax = gdf_bh.plot(
+        color="orange",
+        edgecolor="black",
+        alpha=0.2,
+        figsize=(10, 10)
+    )
+    ax.set_axis_off()
+    
+    cx.add_basemap(
+        ax,
+        source=cx.providers.Stamen.TonerLite
+    )
+
+    return ax
+
+def plot_count_data(df_traffic, ax):
+
+    gdf_traffic = gpd.GeoDataFrame(
+        df_traffic.copy(),
+        geometry=gpd.points_from_xy(
+            df_traffic.LONGITUDE, df_traffic.LATITUDE
+        )
+    )
+
+    gdf_traffic.plot(
+        column="COUNT",
+        legend=True,
+        markersize=1,
+        figsize=(10, 10),
+        ax=ax
+    )
+
+    return ax.figure
 
 
 if __name__ == "__main__":
@@ -105,8 +161,12 @@ if __name__ == "__main__":
     min_date, max_date = dates_range()
     min_hour, max_hour = hour_range()
 
-    df_traffic = read_data(
+    df_traffic = read_traffic_count_data(
         min_date, max_date,
         min_hour, max_hour,
         vehicle_classes
     )
+
+    ax = read_map_data(df_traffic)
+    fig = plot_count_data(df_traffic, ax)
+    st.pyplot(fig)
